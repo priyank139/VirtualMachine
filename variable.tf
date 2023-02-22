432f3f623a4af79f4f996ac8aeed513f9ae71c88
priyank139 <120900797+priyank139@users.noreply.github.com>
2023-02-22 22:31:23 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -44,12 +44,10 @@ variable "azureuser" {
 variable "keydata_path" {
   type = string
 }
-variable "azurekyvaulsecretname" {
-  type = string
-}
-variable "keyvaultid" {
-  type = string
-}
+
+ variable "azurepassword" {
+   type = any
+ }
 
 
 
@@ -60,57 +58,3 @@ variable "keyvaultid" {
 
 
 
-
-
-
-# variable "rglocation" {
-#   type = list(string)
-# }
-
-# variable "resource_group_name" {
-#   type = string
-# }
-
-# variable "public_subnet_id" {
-#   type = list(string)
-# }
-# variable "vm_type" {
-#   type = string
-# }
-
-# variable "public_id_vm" {
-#   type = list(string)
-# }
-
-
-# variable "public_NSG_id" {
-#   type = list(string)
-# }
-# variable "public_natinter_id" {
-#   type = list(string)
-# }
-# variable "publicvm_name" {
-#   type = list(string)
-# }
-# variable "vm_size" {
-#   type = string
-# }
-# variable "skuubuntu" {
-#   type = string
-# }
-# variable "skuwindows" {
-#   type = string
-# }
-# variable "azureuser" {
-#   type = string
-# }
-
-# variable "keydata_path" {
-#   type = string
-# }
-# variable "azurekyvaulsecretname" {
-#   type = string
-# }
-# variable "keyvaultid" {
-#   type = string
-# }
