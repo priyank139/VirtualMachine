989e996bd2875ea58ad7fbcca900e28aac64ef86
priyank139 <120900797+priyank139@users.noreply.github.com>
2023-02-22 22:30:17 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -36,14 +36,6 @@ resource "azurerm_network_interface_security_group_association" "ot_public_assoc
 }
 
 
- data "azurerm_key_vault_secret" "azurepassword" {
-   name         = var.azurekyvaulsecretname #
-   key_vault_id = var.keyvaultid
- }
-
-
-
-
 # ################################ Public Virtual Machine Creation Windows/Ubuntu  ####################
 
 resource "azurerm_linux_virtual_machine" "ot_publicvirtual_machine" {  
@@ -52,7 +44,7 @@ resource "azurerm_linux_virtual_machine" "ot_publicvirtual_machine" {
   location               = element(var.rg_locations, count.index )
   resource_group_name    = var.resource_group_name  
     admin_username =  var.azureuser 
-  admin_password = data.azurerm_key_vault_secret.azurepassword.value 
+  admin_password = var.azurepassword 
   size = var.vm_size
     network_interface_ids = [
     azurerm_network_interface.ot_publican_interface[count.index].id
@@ -89,7 +81,7 @@ resource "azurerm_linux_virtual_machine" "ot_publicvirtual_windowmachine" {
   size = var.vm_size
   resource_group_name    = var.resource_group_name  
   admin_username =  var.azureuser 
-  admin_password = data.azurerm_key_vault_secret.azurepassword.value
+  admin_password = var.azurepassword
   network_interface_ids = [
     azurerm_network_interface.ot_publican_interface[count.index].id
   ]
@@ -141,127 +133,6 @@ resource "azurerm_linux_virtual_machine" "ot_publicvirtual_windowmachine" {
 
 
 
-
-
-
-
-
-
-
-
-# # ####################################### for creating public Static Ip For Virtual Machine  #################
-# resource "azurerm_public_ip" "publicvm_public_ip" {
-#   count = length(var.rglocation)
-#   name                = "publicvm-public-ip${count.index}"
-#   location            = element(var.rglocation, count.index )
-#   resource_group_name = var.resource_group_name
-#   sku = "Standard"
-#   allocation_method   = "Static"
-# }
-
-
-# ########################## Network Interface ############################
-
-# resource "azurerm_network_interface" "ot_publican_interface" {
-#   count                 = length(var.publicvm_name)
-#   name                  = "otservice-public-nic-${count.index}"
-#   location              = element(var.rglocation, count.index )
-#   resource_group_name   = var.resource_group_name
-  
-#  ip_configuration {
-#     name                          = length(var.public_subnet_id)
-#     subnet_id                     = element( var.public_subnet_id, count.index)
-#     private_ip_address_allocation = "Dynamic"
-#     public_ip_address_id          = element( var.public_id_vm, count.index)
-
-#   } 
-# }
-
-# # ###################################### Association Btw Interface and PublicNSG  ####################
-
-# resource "azurerm_network_interface_security_group_association" "ot_public_association" {
-#   count = length(var.public_natinter_id)
-#   network_interface_id  = element(var.public_natinter_id, count.index )
-#   network_security_group_id = element(var.public_NSG_id, count.index )
-# }
-
-
-#  data "azurerm_key_vault_secret" "azurepassword" {
-#    name         = var.azurekyvaulsecretname #
-#    key_vault_id = var.keyvaultid
-#  }
-
-
-
-
-# # ################################ Public Virtual Machine Creation Windows/Ubuntu  ####################
-
-# resource "azurerm_linux_virtual_machine" "ot_publicvirtual_machine" {  
-#   count                  = var.vm_type == "Ubuntu" ? length(var.publicvm_name) : 0
-#   name                   = element(var.publicvm_name, count.index ) 
-#   location               = element(var.rglocation, count.index )
-#   resource_group_name    = var.resource_group_name  
-#     admin_username =  var.azureuser 
-#   admin_password = data.azurerm_key_vault_secret.azurepassword.value 
-#   size = var.vm_size
-#     network_interface_ids = [
-#     azurerm_network_interface.ot_publican_interface[count.index].id
-#   ]
-#  admin_ssh_key {
-#     username   = var.azureuser 
-#     public_key = file("${var.keydata_path}")
-     
-#   }
-# os_disk {
-#     name              = "ubuntupublic-otservice-disk-${count.index}"
-#     caching           = "ReadWrite"
-#     storage_account_type =  "Standard_LRS"
-#   }
-#   source_image_reference {
-#     publisher = "Canonical"
-#     offer     = "0001-com-ubuntu-server-focal"
-#     sku       = var.skuubuntu #
-#     version   = "20.04.202212130"
-
-#   }
-  
-  
-# }
-  
-
-
-
-
-# resource "azurerm_linux_virtual_machine" "ot_publicvirtual_windowmachine" {
-#   count                  = var.vm_type == "Windows" ? length(var.publicvm_name) : 0
-#   name                   = element(var.publicvm_name, count.index ) 
-#   location               = element(var.rglocation, count.index )
-#   size = var.vm_size
-#   resource_group_name    = var.resource_group_name  
-#   admin_username =  var.azureuser 
-#   admin_password = data.azurerm_key_vault_secret.azurepassword.value
-#   network_interface_ids = [
-#     azurerm_network_interface.ot_publican_interface[count.index].id
-#   ]
-
-#  os_disk {
-#     name              = "windows-public-otservice-disk-${count.index}"
-#     caching           = "ReadWrite"
-#     storage_account_type =  "Standard_LRS"
-    
-#   }
-
-#   source_image_reference {
-#     publisher = "MicrosoftWindowsServer"
-#     offer     =  "WindowsServer"
-#     sku       = var.skuwindows 
-#     version   = "latest"
-
-#   }
-  
-# }
-
-# ############################
 
 
 
